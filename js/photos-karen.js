5255cc1597a37103275e4c7a783ed9baac934896
nestd <nestd@users.noreply.github.com>
2026-09-19 21:58:02 -0400

    Add two more photos; light mode only

--- js/photos-karen.js
+++ js/photos-karen.js
@@ -1,20 +1,22 @@
 // To add a photo: put a full-size (max ~1800px) and thumb (~640px) in images/hero/ and add a line here.
 // hero:true photos rotate at the top of the page. All photos appear in the grid, in this order.
 window.PHOTOS = [
-  { src: "images/hero/01.jpg", thumb: "images/hero/01-thumb.jpg", alt: "Photo of Karen (1 of 16)", caption: "", hero: true },
-  { src: "images/hero/13.jpg", thumb: "images/hero/13-thumb.jpg", alt: "Photo of Karen (2 of 16)", caption: "", hero: true },
-  { src: "images/hero/12.jpg", thumb: "images/hero/12-thumb.jpg", alt: "Photo of Karen (3 of 16)", caption: "", hero: true },
-  { src: "images/hero/14.jpg", thumb: "images/hero/14-thumb.jpg", alt: "Photo of Karen (4 of 16)", caption: "", hero: true },
-  { src: "images/hero/02.jpg", thumb: "images/hero/02-thumb.jpg", alt: "Photo of Karen (5 of 16)", caption: "", hero: true },
-  { src: "images/hero/16.jpg", thumb: "images/hero/16-thumb.jpg", alt: "Photo of Karen (6 of 16)", caption: "", hero: true },
-  { src: "images/hero/11.jpg", thumb: "images/hero/11-thumb.jpg", alt: "Photo of Karen (7 of 16)", caption: "", hero: true },
-  { src: "images/hero/04.jpg", thumb: "images/hero/04-thumb.jpg", alt: "Photo of Karen (8 of 16)", caption: "", hero: true },
-  { src: "images/hero/09.jpg", thumb: "images/hero/09-thumb.jpg", alt: "Photo of Karen (9 of 16)", caption: "", hero: true },
-  { src: "images/hero/05.jpg", thumb: "images/hero/05-thumb.jpg", alt: "Photo of Karen (10 of 16)", caption: "", hero: true },
-  { src: "images/hero/10.jpg", thumb: "images/hero/10-thumb.jpg", alt: "Photo of Karen (11 of 16)", caption: "", hero: true },
-  { src: "images/hero/03.jpg", thumb: "images/hero/03-thumb.jpg", alt: "Photo of Karen (12 of 16)", caption: "", hero: true },
-  { src: "images/hero/06.jpg", thumb: "images/hero/06-thumb.jpg", alt: "Photo of Karen (13 of 16)", caption: "", hero: true },
-  { src: "images/hero/07.jpg", thumb: "images/hero/07-thumb.jpg", alt: "Photo of Karen (14 of 16)", caption: "", hero: true },
-  { src: "images/hero/08.jpg", thumb: "images/hero/08-thumb.jpg", alt: "Photo of Karen (15 of 16)", caption: "", hero: true },
-  { src: "images/hero/15.jpg", thumb: "images/hero/15-thumb.jpg", alt: "Photo of Karen (16 of 16)", caption: "" }
+  { src: "images/hero/01.jpg", thumb: "images/hero/01-thumb.jpg", alt: "Photo of Karen (1 of 18)", caption: "", hero: true },
+  { src: "images/hero/13.jpg", thumb: "images/hero/13-thumb.jpg", alt: "Photo of Karen (2 of 18)", caption: "", hero: true },
+  { src: "images/hero/12.jpg", thumb: "images/hero/12-thumb.jpg", alt: "Photo of Karen (3 of 18)", caption: "", hero: true },
+  { src: "images/hero/14.jpg", thumb: "images/hero/14-thumb.jpg", alt: "Photo of Karen (4 of 18)", caption: "", hero: true },
+  { src: "images/hero/02.jpg", thumb: "images/hero/02-thumb.jpg", alt: "Photo of Karen (5 of 18)", caption: "", hero: true },
+  { src: "images/hero/18.jpg", thumb: "images/hero/18-thumb.jpg", alt: "Photo of Karen (6 of 18)", caption: "", hero: true },
+  { src: "images/hero/16.jpg", thumb: "images/hero/16-thumb.jpg", alt: "Photo of Karen (7 of 18)", caption: "", hero: true },
+  { src: "images/hero/11.jpg", thumb: "images/hero/11-thumb.jpg", alt: "Photo of Karen (8 of 18)", caption: "", hero: true },
+  { src: "images/hero/04.jpg", thumb: "images/hero/04-thumb.jpg", alt: "Photo of Karen (9 of 18)", caption: "", hero: true },
+  { src: "images/hero/09.jpg", thumb: "images/hero/09-thumb.jpg", alt: "Photo of Karen (10 of 18)", caption: "", hero: true },
+  { src: "images/hero/05.jpg", thumb: "images/hero/05-thumb.jpg", alt: "Photo of Karen (11 of 18)", caption: "", hero: true },
+  { src: "images/hero/17.jpg", thumb: "images/hero/17-thumb.jpg", alt: "Photo of Karen (12 of 18)", caption: "", hero: true },
+  { src: "images/hero/10.jpg", thumb: "images/hero/10-thumb.jpg", alt: "Photo of Karen (13 of 18)", caption: "", hero: true },
+  { src: "images/hero/03.jpg", thumb: "images/hero/03-thumb.jpg", alt: "Photo of Karen (14 of 18)", caption: "", hero: true },
+  { src: "images/hero/06.jpg", thumb: "images/hero/06-thumb.jpg", alt: "Photo of Karen (15 of 18)", caption: "", hero: true },
+  { src: "images/hero/07.jpg", thumb: "images/hero/07-thumb.jpg", alt: "Photo of Karen (16 of 18)", caption: "", hero: true },
+  { src: "images/hero/08.jpg", thumb: "images/hero/08-thumb.jpg", alt: "Photo of Karen (17 of 18)", caption: "", hero: true },
+  { src: "images/hero/15.jpg", thumb: "images/hero/15-thumb.jpg", alt: "Photo of Karen (18 of 18)", caption: "" }
 ];
